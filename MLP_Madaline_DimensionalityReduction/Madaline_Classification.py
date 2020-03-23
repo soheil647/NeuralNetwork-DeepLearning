06bf86fa749f47d0672d70e11da05a417bb3149d
Soheil <shirvani.soheil647@gmail.com>
2020-03-23 20:08:21 +0430

    TODO:Fashion MINST not completed

--- MLP_Madaline_DimensionalityReduction/Madaline_Classification.py
+++ MLP_Madaline_DimensionalityReduction/Madaline_Classification.py
@@ -1,6 +1,30 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-dataset = np.array(read_train_file())
+Orange_x1 = np.random.normal(3, 0.2, 50)
+Orange_x2 = np.random.normal(2.5, 0.3, 50)
+
+Green_x1 = np.random.normal(1, 0.2, 50)
+Green_x2 = np.random.normal(2, 0.2, 50)
+
+Blue_x1 = [0, 1, 1, 2, 2, 2, 3, 3, 4, 4]
+Blue_x2 = [2, 1, 3.5, 1, 2, 3, 1, 4, 2, 4]
+
+plt.plot(Orange_x1, Orange_x2, "bo")
+plt.plot(Green_x1, Green_x2, "ro")
+plt.plot(Blue_x1, Blue_x2, "yo")
+
+plt.grid()
+# plt.show()
+dataset_list = []
+for i in range(len(Orange_x1)):
+    dataset_list.append([Orange_x1[i], Orange_x2[i], [-1, 1]])
+for i in range(len(Green_x1)):
+    dataset_list.append([Green_x1[i], Green_x2[i], [1, -1]])
+for i in range(len(Blue_x1)):
+    dataset_list.append([Blue_x1[i], Blue_x2[i], [-1, -1]])
+
+# print(np.array(dataset_list).reshape(110, 3))
 
 
 # Make a prediction with weights
@@ -9,33 +33,39 @@ def predict_step(activation):
 
 
 def find_net(in_data, out_weight, out_bias):
+    print(out_bias)
     activation = out_bias
-    for j in range(len(in_data)):
+    for j in range(len(in_data) - 1):
         activation += out_weight[j] * in_data[j]
     return activation
 
 
 # Estimate Perceptron weights using stochastic gradient descent
-def train_weights_MAdaline_MR1(x_in, target, number_of_layer1_neuron, number_of_and_input, l_rate, n_epoch):
-    input_weights_vector = [[0.0 for i in range(len(x_in[0]))] for j in range(len(number_of_layer1_neuron))]
+def train_weights_madaline_mr1(x_in, number_of_layer1_neuron, number_of_and_input, l_rate, n_epoch):
+    input_weights_vector = [[0.0 for i in range(len(x_in[0]) - 1)] for j in range(number_of_layer1_neuron)]
     input_bias_vector = [0.0 for i in range(number_of_layer1_neuron)]
+    # print(np.array(input_weights_vector).reshape(7, 2))
+    # for i in range(number_of_layer1_neuron):
+    # print(np.array(input_bias_vector[1]))
+    and1_weights_vector = [[1, 1, 1, 0, 0, 0, 0], [0, 0, 0, 1, 1, 1, 1]]
+    and1_bias_vector = [-2, -3]
 
-    and1_weights_vector = [[0.0 for i in range(len(number_of_layer1_neuron))] for j in range(len(number_of_and_input))]
-    and1_bias_vector = [0.0 for i in range(len(number_of_and_input))]
-
-    predict_layer1_value = [0.0 for i in range(len(number_of_layer1_neuron))]
-    predict_and_value = [0.0 for i in range(len(number_of_and_input))]
+    predict_layer1_value = [0.0 for i in range(number_of_layer1_neuron)]
+    predict_and_value = [0.0 for i in range(number_of_and_input)]
 
     sum_error = 0
     for i_epoch in range(n_epoch):
-        for data, validate in x_in, target:
+        for data in x_in:
             # for out_put_number in range(len(target)):
             #     predict_layer1_value = []
             #     sum_error = 0.0
+            predict_and_value = []
+            activation_layer1 = []
             for neuron_layer1_number in range(number_of_layer1_neuron):
-                activation_layer1 = find_net(data,
-                                             input_weights_vector[neuron_layer1_number],
-                                             input_bias_vector[neuron_layer1_number])
+                # print(input_bias_vector)
+                activation_layer1[neuron_layer1_number] = find_net(data,
+                                                                   input_weights_vector[neuron_layer1_number],
+                                                                   input_bias_vector[neuron_layer1_number])
                 predict_layer1_value[neuron_layer1_number] = predict_step(activation_layer1)
 
             for and_number in range(number_of_and_input):
@@ -44,29 +74,31 @@ def train_weights_MAdaline_MR1(x_in, target, number_of_layer1_neuron, number_of_
                                           and1_bias_vector[and_number])
                 predict_and_value[and_number] = predict_step(activation_and)
 
-                error = validate[and_number] - predict_and_value[and_number]
-                if error != 0 and validate[and_number] == 1:
+                error = x_in[2][and_number] - predict_and_value[and_number]
+                if error != 0 and x_in[2][and_number] == 1:
                     for neuron_layer1_number in range(number_of_layer1_neuron):
                         if predict_layer1_value[neuron_layer1_number] < 0:
-                            input_weights_vector[neuron_layer1_number] = input_weights_vector[neuron_layer1_number] + l_rate *
+                            input_bias_vector[neuron_layer1_number] = input_bias_vector[neuron_layer1_number] + l_rate * (activation_layer1[number_of_layer1_neuron] - x_in[2][and_number])
+                            for input_number in range(data):
+                                input_weights_vector[neuron_layer1_number][input_number] = input_weights_vector[neuron_layer1_number] + l_rate * (activation_layer1[number_of_layer1_neuron] - x_in[2][and_number]) * data[input_number]
 
-            sum_error += error ** 2
-            if sum_error != 0:
-                bias_vector[outPutNumber] = bias_vector[outPutNumber] + l_rate * row[64 + outPutNumber]
-                for j in range(63):
-                    weights_vector[outPutNumber][j] = weights_vector[outPutNumber][j] + l_rate * row[
-                        64 + outPutNumber] * row[j]
+                if error != 0 and x_in[2][and_number] == -1:
+                    for neuron_layer1_number in range(number_of_layer1_neuron):
+                        if predict_layer1_value[neuron_layer1_number] > 0:
+                            input_bias_vector[neuron_layer1_number] = input_bias_vector[neuron_layer1_number] + l_rate * (activation_layer1[number_of_layer1_neuron] - x_in[2][and_number])
+                            for input_number in range(data):
+                                input_weights_vector[neuron_layer1_number][input_number] = input_weights_vector[neuron_layer1_number] + l_rate * (activation_layer1[number_of_layer1_neuron] - x_in[2][and_number]) * data[input_number]
+                sum_error += error ** 2
         print('>epoch=%d, lrate=%.3f, error=%.3f' % (i_epoch, l_rate, sum_error))
-    return weights_vector, bias_vector
+    return input_weights_vector, input_bias_vector
 
 
 epoch = 4
-weights, bias = train_weights_perception(dataset, 0.2, epoch)
+weights, bias = train_weights_madaline_mr1(x_in=dataset_list, number_of_layer1_neuron=7, number_of_and_input=2, l_rate=0.2, n_epoch=epoch)
 print(weights[0])
 print(bias)
-###############          Enter your code above ...           ##################
-
+# ###############          Enter your code above ...           ##################
+#
 print("\nThe Neural Network has been trained in " + str(epoch) + " epochs.")
-
-###############                   Testing                    ##################
-test_dataset = np.array(read_train_file("OCR_test.txt"))
+#
+# ###############                   Testing                    ##################
---
+++ MLP_Madaline_DimensionalityReduction/predict_house_price.py
@@ -0,0 +1,145 @@
+
+# =============================================================================
+#
+# imports
+#
+# =============================================================================
+
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import datetime
+
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.activations import relu
+from keras.optimizers import adam
+from keras.losses import mean_squared_logarithmic_error
+
+
+def one_layer_network_prediction():
+    # =============================================================================
+    #
+    # reading data
+    #
+    # =============================================================================
+
+    dataset = pd.read_csv("/home/sspc/Desktop/Neural Networks/MLP_Madaline_DimensionalityReduction/House Sales.csv")
+    dataset = dataset.iloc[:5000]
+
+    x_train = dataset.values[:4000, 2:]
+    y_train = dataset.values[:4000, 2]
+
+    x_test = dataset.values[4000:, 2:]
+    y_test = dataset.values[4000:, 2]
+    print(x_test)
+    # =============================================================================
+    #
+    # Model
+    #
+    # =============================================================================
+
+    my_model = Sequential()
+
+    my_model.add(Dense(units=10, activation=relu, input_shape=(19,)))
+    my_model.add(Dense(units=1, activation=relu))
+
+    my_model.compile(optimizer="Adam", loss=mean_squared_logarithmic_error, metrics=["accuracy"])
+
+    start = datetime.datetime.now()
+    my_train = my_model.fit(x=x_train, y=y_train, batch_size=10, epochs=10, validation_split=0.2)
+    end = datetime.datetime.now()
+    Total_time_training = end - start
+
+    # =============================================================================
+    #
+    # test results
+    #
+    # =============================================================================
+
+    history = my_train.history
+    test_loss, test_acc = my_model.evaluate(x=x_test, y=y_test)
+
+    predicted_labels = my_model.predict(x_test)
+    print(history)
+    print(history["loss"])
+    print(history["val_loss"])
+
+    plt.legend("loss")
+    plt.plot(history["loss"])
+
+
+def two_layer_network_prediction():
+    # =============================================================================
+    #
+    # reading data
+    #
+    # =============================================================================
+
+    dataset = pd.read_csv("/home/sspc/Desktop/Neural Networks/MLP_Madaline_DimensionalityReduction/House Sales.csv")
+    dataset = dataset.iloc[:5000]
+
+    x_train = dataset.values[:4000, 2:]
+    y_train = dataset.values[:4000, 2]
+
+    x_test = dataset.values[4000:, 2:]
+    y_test = dataset.values[4000:, 2]
+    print(x_test)
+    # =============================================================================
+    #
+    # Model
+    #
+    # =============================================================================
+
+    my_model = Sequential()
+
+    my_model.add(Dense(units=5, activation=relu, input_shape=(19,)))
+    my_model.add(Dense(units=10, activation=relu))
+    my_model.add(Dense(units=1, activation=relu))
+
+    my_model.compile(optimizer="Adam", loss=mean_squared_logarithmic_error, metrics=["accuracy"])
+
+    my_train = my_model.fit(x=x_train, y=y_train, batch_size=10, epochs=10, validation_split=0.2)
+
+    # =============================================================================
+    #
+    # test results
+    #
+    # =============================================================================
+
+    history = my_train.history
+    test_loss, test_acc = my_model.evaluate(x=x_test, y=y_test)
+
+    predicted_labels = my_model.predict(x_test)
+    print(history)
+    print(history["loss"])
+    print(history["val_loss"])
+
+    plt.legend(["loss"])
+    plt.plot(history["loss"])
+
+
+# =============================================================================
+#
+# MLP with 1 hidden layer
+#
+# =============================================================================
+
+start = datetime.datetime.now()
+one_layer_network_prediction()
+end = datetime.datetime.now()
+print(end - start)
+
+# =============================================================================
+#
+# MLP with 2 hidden layer
+#
+# =============================================================================
+
+start = datetime.datetime.now()
+two_layer_network_prediction()
+end = datetime.datetime.now()
+print(end - start)
+
+plt.legend(["1 layer loss", "2 layer loss"])
+plt.show()
